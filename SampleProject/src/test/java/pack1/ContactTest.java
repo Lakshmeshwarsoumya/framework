62b2d21bc4fa65a5dc6e1d9d354c1af62043cf8c
soumya <saumya8226@gmail.com>
2025-03-05 15:34:26 +0530

    Committed

---
+++ SampleProject/src/test/java/pack1/ContactTest.java
@@ -0,0 +1,27 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContactTest() {
+
+		String URL = System.getProperty("url");
+		String Browser = System.getProperty("browser");
+		String Username = System.getProperty("username");
+		String Password = System.getProperty("Password");
+
+		System.out.println("execute createcontacttest");
+		System.out.println(URL);
+		System.out.println(Browser);
+		System.out.println(Username);
+		System.out.println(Password);
+	}
+
+	@Test
+	public void modifyContactTest() {
+
+		System.out.println("execute modifycontacttest");
+	}
+
+}
